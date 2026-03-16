124ddf7ecc806bfb07761ae757bbf4d8b70bdc92
Martin Pellizzer <martinpellizzer@gmail.com>
2026-03-16 16:23:37 +0100

    '

--- chemistry.py
+++ chemistry.py
@@ -27,9 +27,37 @@ intensive property vs extensive property
 scientific notation
 ---
 significant figures
+accuracy vs precision
+
+units and conversions
+---
+SI Units
+
+length:         m
+mass:           kg (g)
+time:           s
+temperature:    K
+volume:         m3 (L)
+energy/work:    J
+pressure:       Pa (atm)
+
+density
 
 '''
 
 from lib import calc
 
 print(calc.calc_scientific_notation(472000))
+print('g_to_tg:', calc.g_to_tg(75), 'tg')
+print('g_to_gg:', calc.g_to_gg(75), 'gg')
+print('g_to_Mg:', calc.g_to_Mg(75), 'Mg')
+print('g_to_kg:', calc.g_to_kg(75), 'kg')
+print('g_to_mg:', calc.g_to_mg(75), 'mg')
+print('in_to_cm:', calc.in_to_cm(1), 'cm')
+print('m_to_ft:', calc.m_to_ft(17.1), 'ft')
+print('celsius_to_kelvin:', calc.celsius_to_kelvin(10), 'K')
+print('kelvin_to_celsius:', calc.kelvin_to_celsius(10), 'C')
+print('density:', calc.density(12, 8), 'g/cm3')
+print('mass:', calc.mass(1.5, 27), 'g')
+
+
--- lib/calc.py
+++ lib/calc.py
@@ -11,4 +11,56 @@ def calc_scientific_notation(number, decimals=3):
     mantissa_str = f"{mantissa:.{decimals}f}"
     return f"{mantissa_str} × 10^{exponent}"
 
+################################################################################
+# UNITS AND CONVERSIONS
+################################################################################
+
+def g_to_tg(val):
+    res = val / 1000 / 1000 / 1000 / 1000
+    return res
+
+def g_to_gg(val):
+    res = val / 1000 / 1000 / 1000
+    return res
+
+def g_to_Mg(val):
+    res = val / 1000 / 1000
+    return res
+
+def g_to_kg(val):
+    res = val / 1000
+    return res
+
+def g_to_mg(val):
+    res = val * 1000
+    return res
+
+# CANONICAL
+def in_to_cm(val):
+    res = val * 2.54
+    return res
+
+def m_to_ft(val):
+    res = val * 100 / 2.54 / 12
+    return res
+
+# TEMPERATURE
+# ---
+
+def celsius_to_kelvin(val):
+    res = val + 273
+    return res
+
+def kelvin_to_celsius(val):
+    res = val - 273
+    return res
+
+def density(m, V):
+    # letter ro (look like a "p") is the ufficial symbol for density
+    res = m / V
+    return res
+
+def mass(d, V):
+    res = d * V
+    return res
 
